164d107d65f70a4e42917ef249979fac7ce59ccc
nadavkli <nadavk@realfriend.ai>
2024-05-08 15:06:49 +0300

    feat: v1.0.1

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -1,11 +1,11 @@
 import {
   KeyboardAccessoryView,
-  useComponentSize
-} from "@flyerhq/react-native-keyboard-accessory-view";
-import { oneOf } from "@flyerhq/react-native-link-preview";
-import dayjs from "dayjs";
-import calendar from "dayjs/plugin/calendar";
-import * as React from "react";
+  useComponentSize,
+} from '@flyerhq/react-native-keyboard-accessory-view'
+import { oneOf } from '@flyerhq/react-native-link-preview'
+import dayjs from 'dayjs'
+import calendar from 'dayjs/plugin/calendar'
+import * as React from 'react'
 import {
   FlatList,
   FlatListProps,
@@ -15,42 +15,42 @@ import {
   StatusBar,
   StatusBarProps,
   Text,
-  View
-} from "react-native";
-import { useSafeAreaInsets } from "react-native-safe-area-context";
+  View,
+} from 'react-native'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
 
-import { usePrevious } from "../../hooks";
-import { l10n } from "../../l10n";
-import { defaultTheme } from "../../theme";
-import { MessageType, Theme, User } from "../../types";
+import { usePrevious } from '../../hooks'
+import { l10n } from '../../l10n'
+import { defaultTheme } from '../../theme'
+import { MessageType, Theme, User } from '../../types'
 import {
   calculateChatMessages,
   initLocale,
   L10nContext,
   ThemeContext,
   unwrap,
-  UserContext
-} from "../../utils";
-import { CircularActivityIndicator } from "../CircularActivityIndicator";
-import { Input, InputAdditionalProps, InputTopLevelProps } from "../Input";
-import { Message, MessageTopLevelProps } from "../Message";
-import ImageView from "./ImageView";
-import styles from "./styles";
+  UserContext,
+} from '../../utils'
+import { CircularActivityIndicator } from '../CircularActivityIndicator'
+import { Input, InputAdditionalProps, InputTopLevelProps } from '../Input'
+import { Message, MessageTopLevelProps } from '../Message'
+import ImageView from './ImageView'
+import styles from './styles'
 
 // Untestable
 /* istanbul ignore next */
 const animate = () => {
-  LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
-};
+  LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut)
+}
 
 // eslint-disable-next-line jest/require-hook
-dayjs.extend(calendar);
+dayjs.extend(calendar)
 
 export type ChatTopLevelProps = InputTopLevelProps & MessageTopLevelProps
 
 export interface ChatProps extends ChatTopLevelProps {
   /** Allows you to replace the default Input widget e.g. if you want to create a channel view. */
-  customBottomComponent?: () => React.ReactNode;
+  customBottomComponent?: () => React.ReactNode
   /** If {@link ChatProps.dateFormat} and/or {@link ChatProps.timeFormat} is not enough to
    * customize date headers in your case, use this to return an arbitrary
    * string based on a `dateTime` of a particular message. Can be helpful to
@@ -58,83 +58,83 @@ export interface ChatProps extends ChatTopLevelProps {
    * all default date headers, so you must handle all cases yourself, like
    * for example today, yesterday and before. Or you can just return the same
    * date header for any message. */
-  customDateHeaderText?: (dateTime: number) => string;
+  customDateHeaderText?: (dateTime: number) => string
   /** Allows you to customize the date format. IMPORTANT: only for the date,
    * do not return time here. @see {@link ChatProps.timeFormat} to customize the time format.
    * @see {@link ChatProps.customDateHeaderText} for more customization. */
-  dateFormat?: string;
+  dateFormat?: string
   /** Disable automatic image preview on tap. */
-  disableImageGallery?: boolean;
+  disableImageGallery?: boolean
   /** Allows you to change what the user sees when there are no messages.
    * `emptyChatPlaceholder` and `emptyChatPlaceholderTextStyle` are ignored
    * in this case. */
-  emptyState?: () => React.ReactNode;
+  emptyState?: () => React.ReactNode
   /** Use this to enable `LayoutAnimation`. Experimental on Android (same as React Native). */
-  enableAnimation?: boolean;
-  flatListProps?: Partial<FlatListProps<MessageType.DerivedAny[]>>;
-  inputProps?: InputAdditionalProps;
+  enableAnimation?: boolean
+  flatListProps?: Partial<FlatListProps<MessageType.DerivedAny[]>>
+  inputProps?: InputAdditionalProps
   /** Used for pagination (infinite scroll) together with {@link ChatProps.onEndReached}.
    * When true, indicates that there are no more pages to load and
    * pagination will not be triggered. */
-  isLastPage?: boolean;
+  isLastPage?: boolean
   /** Override the default localized copy. */
-  l10nOverride?: Partial<Record<keyof typeof l10n[keyof typeof l10n], string>>;
-  locale?: keyof typeof l10n;
-  messages: MessageType.Any[];
+  l10nOverride?: Partial<Record<keyof typeof l10n[keyof typeof l10n], string>>
+  locale?: keyof typeof l10n
+  messages: MessageType.Any[]
   /** Used for pagination (infinite scroll). Called when user scrolls
    * to the very end of the list (minus `onEndReachedThreshold`).
    * See {@link ChatProps.flatListProps} to set it up. */
-  onEndReached?: () => Promise<void>;
+  onEndReached?: () => Promise<void>
   /** Show user names for received messages. Useful for a group chat. Will be
    * shown only on text messages. */
-  showUserNames?: boolean;
+  showUserNames?: boolean
   /** Chat theme. Implement {@link Theme} to create your own theme or use
    * existing one, like the {@link defaultTheme}. */
-  theme?: Theme;
+  theme?: Theme
   /**
    * Allows you to customize the time format. IMPORTANT: only for the time,
    * do not return date here. @see {@link ChatProps.dateFormat} to customize the date format.
    * @see {@link ChatProps.customDateHeaderText} for more customization.
    */
-  timeFormat?: string;
-  user: User;
+  timeFormat?: string
+  user: User
 }
 
 /** Entry component, represents the complete chat */
 export const Chat = ({
-                       customBottomComponent,
-                       customDateHeaderText,
-                       dateFormat,
-                       disableImageGallery,
-                       emptyState,
-                       enableAnimation,
-                       flatListProps,
-                       inputProps,
-                       isAttachmentUploading,
-                       isLastPage,
-                       l10nOverride,
-                       locale = "en",
-                       messages,
-                       onAttachmentPress,
-                       onEndReached,
-                       onMessageLongPress,
-                       onMessagePress,
-                       onPreviewDataFetched,
-                       onSendPress,
-                       renderBubble,
-                       renderCustomMessage,
-                       renderFileMessage,
-                       renderImageMessage,
-                       renderTextMessage,
-                       sendButtonVisibilityMode = "editing",
-                       showUserAvatars = false,
-                       showUserNames = false,
-                       textInputProps,
-                       theme = defaultTheme,
-                       timeFormat,
-                       usePreviewData = true,
-                       user
-                     }: ChatProps) => {
+  customBottomComponent,
+  customDateHeaderText,
+  dateFormat,
+  disableImageGallery,
+  emptyState,
+  enableAnimation,
+  flatListProps,
+  inputProps,
+  isAttachmentUploading,
+  isLastPage,
+  l10nOverride,
+  locale = 'en',
+  messages,
+  onAttachmentPress,
+  onEndReached,
+  onMessageLongPress,
+  onMessagePress,
+  onPreviewDataFetched,
+  onSendPress,
+  renderBubble,
+  renderCustomMessage,
+  renderFileMessage,
+  renderImageMessage,
+  renderTextMessage,
+  sendButtonVisibilityMode = 'editing',
+  showUserAvatars = false,
+  showUserNames = false,
+  textInputProps,
+  theme = defaultTheme,
+  timeFormat,
+  usePreviewData = true,
+  user,
+}: ChatProps) => {
   const {
     container,
     emptyComponentContainer,
@@ -144,65 +144,65 @@ export const Chat = ({
     footer,
     footerLoadingPage,
     header,
-    keyboardAccessoryView
-  } = styles({ theme });
+    keyboardAccessoryView,
+  } = styles({ theme })
 
-  const { onLayout, size } = useComponentSize();
-  const animationRef = React.useRef(false);
-  const list = React.useRef<FlatList<MessageType.DerivedAny>>(null);
-  const insets = useSafeAreaInsets();
-  const [isImageViewVisible, setIsImageViewVisible] = React.useState(false);
-  const [isNextPageLoading, setNextPageLoading] = React.useState(false);
-  const [imageViewIndex, setImageViewIndex] = React.useState(0);
-  const [stackEntry, setStackEntry] = React.useState<StatusBarProps>({});
+  const { onLayout, size } = useComponentSize()
+  const animationRef = React.useRef(false)
+  const list = React.useRef<FlatList<MessageType.DerivedAny>>(null)
+  const insets = useSafeAreaInsets()
+  const [isImageViewVisible, setIsImageViewVisible] = React.useState(false)
+  const [isNextPageLoading, setNextPageLoading] = React.useState(false)
+  const [imageViewIndex, setImageViewIndex] = React.useState(0)
+  const [stackEntry, setStackEntry] = React.useState<StatusBarProps>({})
 
   const l10nValue = React.useMemo(
     () => ({ ...l10n[locale], ...unwrap(l10nOverride) }),
     [l10nOverride, locale]
-  );
+  )
 
   const { chatMessages, gallery } = calculateChatMessages(messages, user, {
     customDateHeaderText,
     dateFormat,
     showUserNames,
-    timeFormat
-  });
+    timeFormat,
+  })
 
-  const previousChatMessages = usePrevious(chatMessages);
+  const previousChatMessages = usePrevious(chatMessages)
 
   React.useEffect(() => {
     if (
-      chatMessages[0]?.type !== "dateHeader" &&
+      chatMessages[0]?.type !== 'dateHeader' &&
       chatMessages[0]?.id !== previousChatMessages?.[0]?.id &&
       chatMessages[0]?.author?.id === user.id
     ) {
       list.current?.scrollToOffset({
         animated: true,
-        offset: 0
-      });
+        offset: 0,
+      })
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [chatMessages]);
+  }, [chatMessages])
 
   React.useEffect(() => {
-    initLocale(locale);
-  }, [locale]);
+    initLocale(locale)
+  }, [locale])
 
   // Untestable
   /* istanbul ignore next */
   if (animationRef.current && enableAnimation) {
-    InteractionManager.runAfterInteractions(animate);
+    InteractionManager.runAfterInteractions(animate)
   }
 
   React.useEffect(() => {
     // Untestable
     /* istanbul ignore next */
     if (animationRef.current && enableAnimation) {
-      InteractionManager.runAfterInteractions(animate);
+      InteractionManager.runAfterInteractions(animate)
     } else {
-      animationRef.current = true;
+      animationRef.current = true
     }
-  }, [enableAnimation, messages]);
+  }, [enableAnimation, messages])
 
   const handleEndReached = React.useCallback(
     // Ignoring because `scroll` event for some reason doesn't trigger even basic
@@ -217,15 +217,15 @@ export const Chat = ({
         messages.length === 0 ||
         isNextPageLoading
       ) {
-        return;
+        return
       }
 
-      setNextPageLoading(true);
-      await onEndReached?.();
-      setNextPageLoading(false);
+      setNextPageLoading(true)
+      await onEndReached?.()
+      setNextPageLoading(false)
     },
     [isLastPage, isNextPageLoading, messages.length, onEndReached]
-  );
+  )
 
   const handleImagePress = React.useCallback(
     (message: MessageType.Image) => {
@@ -233,56 +233,56 @@ export const Chat = ({
         gallery.findIndex(
           (image) => image.id === message.id && image.uri === message.uri
         )
-      );
-      setIsImageViewVisible(true);
+      )
+      setIsImageViewVisible(true)
       setStackEntry(
         StatusBar.pushStackEntry({
-          barStyle: "light-content",
-          animated: true
+          barStyle: 'light-content',
+          animated: true,
         })
-      );
+      )
     },
     [gallery]
-  );
+  )
 
   const handleMessagePress = React.useCallback(
     (message: MessageType.Any) => {
-      if (message.type === "image" && !disableImageGallery) {
-        handleImagePress(message);
+      if (message.type === 'image' && !disableImageGallery) {
+        handleImagePress(message)
       }
-      onMessagePress?.(message);
+      onMessagePress?.(message)
     },
     [disableImageGallery, handleImagePress, onMessagePress]
-  );
+  )
 
   // TODO: Tapping on a close button results in the next warning:
   // `An update to ImageViewing inside a test was not wrapped in act(...).`
   /* istanbul ignore next */
   const handleRequestClose = () => {
-    setIsImageViewVisible(false);
-    StatusBar.popStackEntry(stackEntry);
-  };
+    setIsImageViewVisible(false)
+    StatusBar.popStackEntry(stackEntry)
+  }
 
   const keyExtractor = React.useCallback(
     ({ id }: MessageType.DerivedAny) => id,
     []
-  );
+  )
 
   const renderItem = React.useCallback(
     ({ item: message }: { item: MessageType.DerivedAny; index: number }) => {
       const messageWidth =
         showUserAvatars &&
-        message.type !== "dateHeader" &&
+        message.type !== 'dateHeader' &&
         message.author.id !== user.id
           ? Math.floor(Math.min(size.width * 0.72, 440))
-          : Math.floor(Math.min(size.width * 0.77, 440));
+          : Math.floor(Math.min(size.width * 0.77, 440))
 
       const roundBorder =
-        message.type !== "dateHeader" && message.nextMessageInGroup;
+        message.type !== 'dateHeader' && message.nextMessageInGroup
       const showAvatar =
-        message.type !== "dateHeader" && !message.nextMessageInGroup;
-      const showName = message.type !== "dateHeader" && message.showName;
-      const showStatus = message.type !== "dateHeader" && message.showStatus;
+        message.type !== 'dateHeader' && !message.nextMessageInGroup
+      const showName = message.type !== 'dateHeader' && message.showName
+      const showStatus = message.type !== 'dateHeader' && message.showStatus
 
       return (
         <Message
@@ -303,10 +303,10 @@ export const Chat = ({
             showName,
             showStatus,
             showUserAvatars,
-            usePreviewData
+            usePreviewData,
           }}
         />
-      );
+      )
     },
     [
       enableAnimation,
@@ -321,9 +321,9 @@ export const Chat = ({
       showUserAvatars,
       size.width,
       usePreviewData,
-      user.id
+      user.id,
     ]
-  );
+  )
 
   const renderListEmptyComponent = React.useCallback(
     () => (
@@ -337,7 +337,7 @@ export const Chat = ({
       </View>
     ),
     [emptyComponentContainer, emptyComponentTitle, emptyState, l10nValue]
-  );
+  )
 
   const renderListFooterComponent = React.useCallback(
     () =>
@@ -351,7 +351,7 @@ export const Chat = ({
         <View style={footer} />
       ),
     [footer, footerLoadingPage, isNextPageLoading, theme.colors.primary]
-  );
+  )
 
   const renderScrollable = React.useCallback(
     (panHandlers: GestureResponderHandlers) => (
@@ -361,9 +361,10 @@ export const Chat = ({
           flatListContentContainer,
           // eslint-disable-next-line react-native/no-inline-styles
           {
-            justifyContent: chatMessages.length !== 0 ? "flex-end" : "center",
-            paddingTop: insets.bottom
-          }
+            flexGrow: 1,
+            justifyContent: chatMessages.length !== 0 ? 'flex-end' : 'center',
+            paddingTop: insets.bottom,
+          },
         ]}
         initialNumToRender={10}
         ListEmptyComponent={renderListEmptyComponent}
@@ -377,7 +378,7 @@ export const Chat = ({
         {...unwrap(flatListProps)}
         data={chatMessages}
         inverted
-        keyboardDismissMode="interactive"
+        keyboardDismissMode='interactive'
         keyExtractor={keyExtractor}
         onEndReached={handleEndReached}
         ref={list}
@@ -396,9 +397,9 @@ export const Chat = ({
       keyExtractor,
       renderItem,
       renderListEmptyComponent,
-      renderListFooterComponent
+      renderListFooterComponent,
     ]
-  );
+  )
 
   return (
     <UserContext.Provider value={user}>
@@ -414,7 +415,7 @@ export const Chat = ({
               <KeyboardAccessoryView
                 {...{
                   renderScrollable,
-                  style: keyboardAccessoryView
+                  style: keyboardAccessoryView,
                 }}
               >
                 <Input
@@ -425,7 +426,7 @@ export const Chat = ({
                     onSendPress,
                     renderScrollable,
                     sendButtonVisibilityMode,
-                    textInputProps
+                    textInputProps,
                   }}
                 />
               </KeyboardAccessoryView>
@@ -440,5 +441,5 @@ export const Chat = ({
         </L10nContext.Provider>
       </ThemeContext.Provider>
     </UserContext.Provider>
-  );
-};
+  )
+}
